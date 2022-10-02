5b41a8439b65a0ed5c2e50cf2dc145979e59f03c
AmitJalal <amitjalal53@gmail.com>
2022-10-03 00:30:32 +0530

    patterns

--- Patterns/number.py
+++ Patterns/number.py
@@ -17,3 +17,25 @@ n=5 -->
     44444
     55555
 '''
+
+
+print() # new b/w different o/p
+
+row = 1
+while row <= n :
+    col = n 
+    while col>0:
+        print(col, end='')
+        col-=1
+    print()    
+    row+=1
+
+'''
+output : n = 4
+
+4321
+4321
+4321
+4321
+
+'''    
