ec9d57f4bf3c02325ba7c7ca7c0bdfef4f5cae13
AmitJalal <amitjalal53@gmail.com>
2023-03-21 23:12:28 +0530

    function practice in python

---
+++ functions/fun_prac.py
@@ -0,0 +1,36 @@
+def life_span_macbook(list):
+    sum = 0
+    for ele in list:
+        if ele > 0:
+            sum += ele
+    return sum
+
+
+# input
+n = int(input())
+list = []
+for i in range(0, n):
+    list.append(int(input()))
+
+# output
+print(life_span_macbook(list))
+
+
+# =========================
+
+
+def special_range(s, e):
+    if e < 0 or (s > 0 and e < 0) or (s > 0 and e < s):
+        print(-1)
+        return
+
+    if s < 0 and e >= 0:
+        s = 0
+    for i in range(s, e + 1):
+        print(i)
+
+
+# input
+start = int(input())
+end = int(input())
+special_range(start, end)
