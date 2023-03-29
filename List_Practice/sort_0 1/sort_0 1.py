e958b98abe8cc4399f568547c2dcba3677f977a9
AmitJalal <amitjalal53@gmail.com>
2023-03-30 02:48:04 +0530

    sort_0_1 log(N) and O(N) soln

--- List_Practice/sort_0 1/sort_0 1.py
+++ List_Practice/sort_0 1/sort_0 1.py
@@ -1,3 +1,7 @@
+# ==================================
+#    --->  log(N)
+# ==================================
+
 def sort_zero_one(list):
     start = 0
     end = len(list) - 1
@@ -17,8 +21,23 @@ def sort_zero_one(list):
 
     return list
 
+# =============================
+#         -->  O(N)
+# =============================
+
+def sort_zero_one(list):
+    zeroPos = 0
+    current = 0
+    while current < len(list):
+        if list[current] == 0:
+            list[current], list[zeroPos] = list[zeroPos], list[current]
+            zeroPos = zeroPos + 1
+        current = current + 1
+
+    return list
+
 
 # 1 0 1 1 0 1 0 1
-# print(sort_zero_one([1, 0, 1, 1, 0, 1, 0, 1])) # [0, 0, 0, 1, 1, 1, 1, 1]
+print(sort_zero_one([1, 0, 1, 1, 0, 1, 0, 1]))  # [0, 0, 0, 1, 1, 1, 1, 1]
 
-# print(sort_zero_one([0, 1, 1, 0, 1, 0, 1]))  # [0, 0, 0, 1, 1, 1, 1]
+print(sort_zero_one([0, 1, 1, 0, 1, 0, 1]))  # [0, 0, 0, 1, 1, 1, 1]
