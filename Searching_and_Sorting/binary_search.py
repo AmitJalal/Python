c06c9480ac834773866d5fa0010b8f99340b88b8
AmitJalal <amitjalal53@gmail.com>
2023-03-30 04:33:17 +0530

    optimized the binary search

--- Searching_and_Sorting/binary_search.py
+++ Searching_and_Sorting/binary_search.py
@@ -1,5 +1,6 @@
 # =========================
-#   My Implementation more optimized than online solns
+#   My Implementation more optimized than online solns --> tried optimizing the binary search
+#   So far I have run through many testcases
 # =========================
 
 
@@ -9,6 +10,8 @@ def search_ele(list, ele):
     # loop_count = 0
     while start < end:
         mid = (start + end) // 2
+        if list[end] < ele or list[start] > ele:
+            return -1
         if list[start] == ele:
             return start
         if list[mid] == ele:
@@ -19,13 +22,13 @@ def search_ele(list, ele):
             start = mid + 1
         if list[mid] > ele:
             end = mid - 1
-        loop_count += 1
+        # loop_count += 1
     # print(f"loop_count: {loop_count}")
     return -1
 
 
 # =========================
-#   Refactored but i guess mine is faster at time compelexity
+#   Refactored(Traditional Way) but i guess mine is faster at time compelexity
 # =========================
 
 
@@ -41,18 +44,26 @@ def search_ele(list, ele):
             end = mid - 1
         else:
             return mid
-        loop_count += 1
+        # loop_count += 1
     # print(f"loop_count: {loop_count}")
     return -1
 
 
-print(search_ele([1, 3, 7, 9, 11, 12, 45], 11))  # 4
-print(search_ele([1, 3, 7, 9, 11, 12, 45], 1))  # 0
-print(search_ele([1, 3, 7, 9, 11, 12, 45], 2))  # -1
-print(search_ele([1, 3, 7, 9, 11, 12, 45], 80))  # -1
-print(search_ele([1, 3, 7, 9, 11, 12, 45], 12))  # 5
-print(search_ele([1, 2, 3, 4, 5, 6], 9))  # -1
-print(search_ele([1, 2, 3, 4, 5, 6], 6))  # 5
+# print(search_ele([0, 2, 4, 6, 8, 10, 12, 14, 16, 18], 9))  # -1
+# print(search_ele([0, 2, 4, 6, 8, 10, 12, 14, 16, 18], 0))  # 0
+# print(search_ele([0, 2, 4, 6, 8, 10, 12, 14, 16, 18], 16))  # 0
+# print(search_ele([1, 2, 4, 7, 8, 12, 15, 19, 24, 50, 69, 80, 100], 16))  # -1
+# print(search_ele([1, 2, 4, 7, 8, 12, 15, 19, 24, 50, 69, 80, 100], 69))  # 10
+# print(search_ele([1, 2, 4, 7, 8, 12, 15, 19, 24, 50, 69, 80, 100], 12))  # 5
+# print(search_ele([1, 3, 7, 9, 11, 12, 45], 11))  # 4
+# print(search_ele([1, 3, 7, 9, 11, 12, 45], 1))  # 0
+# print(search_ele([1, 3, 7, 9, 11, 12, 45], 2))  # -1
+# print(search_ele([1, 3, 7, 9, 11, 12, 45], 80))  # -1
+# print(search_ele([1, 3, 7, 9, 11, 12, 45], 12))  # 5
+# print(search_ele([1, 2, 3, 4, 5, 6], 9))  # -1
+# print(search_ele([1, 2, 3, 4, 5, 6], 6))  # 5
+
+
 
 
 # 1 3 7 9 11 12 45  # --> 9 -> mid= 3 -> arr[mid] == 9 --> return mid
