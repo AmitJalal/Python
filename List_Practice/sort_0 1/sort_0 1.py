014c2f318c868f314df52c556e260473a4bb5ada
AmitJalal <amitjalal53@gmail.com>
2023-03-30 02:29:37 +0530

    sort 0 and 1

---
+++ List_Practice/sort_0 1/sort_0 1.py
@@ -0,0 +1,24 @@
+def sort_zero_one(list):
+    start = 0
+    end = len(list) - 1
+    while start <= end:
+        if list[start] == list[end]:
+            if list[start] == 0:
+                start += 1
+            if list[start] == 1:
+                end -= 1
+        elif list[start] < list[end]:
+            start += 1
+            end -= 1
+        elif list[start] > list[end]:
+            list[start], list[end] = list[end], list[start]
+            start += 1
+            end -= 1
+
+    return list
+
+
+# 1 0 1 1 0 1 0 1
+# print(sort_zero_one([1, 0, 1, 1, 0, 1, 0, 1])) # [0, 0, 0, 1, 1, 1, 1, 1]
+
+# print(sort_zero_one([0, 1, 1, 0, 1, 0, 1]))  # [0, 0, 0, 1, 1, 1, 1]
