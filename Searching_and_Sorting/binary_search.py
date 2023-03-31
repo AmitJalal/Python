2766048214bcbba8dcba2f8ffed1ce743b9f6e87
AmitJalal <amitjalal53@gmail.com>
2023-04-01 02:32:39 +0530

    binary search

--- Searching_and_Sorting/binary_search.py
+++ Searching_and_Sorting/binary_search.py
@@ -38,12 +38,12 @@ def search_ele(list, ele):
     # loop_count = 0
     while start <= end:
         mid = (start + end) // 2
-        if list[mid] < ele:
-            start = mid + 1
-        elif list[mid] > ele:
-            end = mid - 1
-        else:
+        if list[mid] == ele:
             return mid
+        elif list[mid] < ele:
+            start = mid + 1
+        else:
+            end = mid - 1
         # loop_count += 1
     # print(f"loop_count: {loop_count}")
     return -1
@@ -62,8 +62,6 @@ print(search_ele([1, 3, 7, 9, 11, 12, 45], 80))  # -1
 print(search_ele([1, 3, 7, 9, 11, 12, 45], 12))  # 5
 print(search_ele([1, 2, 3, 4, 5, 6], 9))  # -1
 print(search_ele([1, 2, 3, 4, 5, 6], 6))  # 5
-
-
 
 
 # 1 3 7 9 11 12 45  # --> 9 -> mid= 3 -> arr[mid] == 9 --> return mid
